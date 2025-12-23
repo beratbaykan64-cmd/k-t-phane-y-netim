6825d1da5e1623a6b73257f2ad170112fdb2182b
beratbaykan64-cmd <beratbaykan64@gmail.com>
2025-12-24 01:21:36 +0300

    Veritabani kodlari eklendi

--- KutuphaneProjesi/KutuphaneYonetim/Program.cs
+++ KutuphaneProjesi/KutuphaneYonetim/Program.cs
@@ -3,23 +3,32 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Veritabanı Bağlantısı
+// 1. Veritabanı Bağlantısı (SQLite)
 builder.Services.AddDbContext<UygulamaDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// MVC Servisleri
+// 2. MVC Servisleri
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Hata Yönetimi ve HTTPS
+// --- EKSİK OLAN KISIM BURASI ---
+// Veritabanını otomatik oluşturacak kod:
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<UygulamaDbContext>();
+    db.Database.EnsureCreated();
+}
+// -------------------------------
+
+// Hata Yönetimi
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
